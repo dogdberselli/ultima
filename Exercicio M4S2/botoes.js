ef7d017cdc8417fca7ce09e3a72618945b340908
dogdberselli <doglasberselli@hotmail.com>
2023-08-07 17:52:51 -0300

    primeiro

---
+++ Exercicio M4S2/botoes.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var botao1 = document.getElementById("botao1");
+  var botao2 = document.getElementById("botao2");
+  var botao3 = document.getElementById("botao3");
+  var botao4 = document.getElementById("botao4");
+  
+  botao1.addEventListener("click", function() {
+      alert("Produto em falta");
+  });
+  
+  botao2.addEventListener("click", function() {
+      alert("Produto em falta");
+  });
+  
+  botao3.addEventListener("click", function() {
+      alert("Produto em falta");
+  });
+
+  botao4.addEventListener("click", function() {
+    alert("Produto em falta");
+});
+});
