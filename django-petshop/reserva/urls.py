38dc4382ec786442c2ac747287cfc0760ca2c714
dogdberselli <131552380+dogdberselli@users.noreply.github.com>
2023-11-02 22:17:06 -0300

    Delete django-petshop directory

--- django-petshop/reserva/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from reserva.views import criar_reserva
-
-app_name = 'reserva'
-
-urlpatterns = [
-    path('criar/',criar_reserva,name='criar_reserva'),
-]
--- django-petshop/rest_api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from reserva.models import Reserva
-
-class AgendamentoModelSerializer(ModelSerializer):
-    class Meta:
-        model = Reserva
-        fields = '__all__'
-    pass
